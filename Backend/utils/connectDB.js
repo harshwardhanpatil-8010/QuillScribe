52862bea0a9b1daaf688f64583f0ebb310e28724
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-04-27 00:38:43 +0530

    new

--- Backend/utils/connectDB.js
+++ Backend/utils/connectDB.js
@@ -1,17 +1,15 @@
-// backend/utils/connectDB.js
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    if (mongoose.connections[0].readyState) {
-      console.log("Already connected to MongoDB");
-      return;
-    }
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB ✅");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected!");
   } catch (error) {
-    console.error("MongoDB Connection Error ❌", error);
-    throw new Error(error);
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
   }
 };
 
